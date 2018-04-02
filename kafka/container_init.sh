6d43577295ffa8f714b6121afe8af83d6fc98ed0
Eduardo A. Paris Penas <edward2a@gmail.com>
2018-04-02 12:55:55 +0100

    update kafka container to use eth0 IP by default and expose 9092

--- kafka/container_init.sh
+++ kafka/container_init.sh
@@ -1,16 +1,34 @@
 #!/bin/sh
+set -eu
 
 set ZOO_PID
 set KFK_PID
 
+NET_IF=${NET_IF:-eth0}
+NET_PORT=${NET_PORT:-9092}
+ZOO_CONFIG=/opt/kafka/config/zookeeper.properties
+KFK_CONFIG=/opt/kafka/config/server.properties
+
+function configure_kafka_ip(){
+    local NET_ADDR
+
+    NET_ADDR=$(ip addr show dev ${NET_IF} | pcregrep -o 'inet \K[\d\.]+')
+    sed -i -e "s|^host\.name=.*$|host.name=${NET_ADDR}|" ${KFK_CONFIG}
+    sed -i -e "s|^advertised\.host\.name=.*$|advertised.host.name=${NET_ADDR}|" ${KFK_CONFIG}
+    sed -i -e "s|^listeners=.*$|listeners=PLAINTEXT://${NET_ADDR}:${NET_PORT}|" ${KFK_CONFIG}
+    sed -i -e "s|^advertised.listeners=.*$|advertised.listeners=PLAINTEXT://${NET_ADDR}:${NET_PORT}|" ${KFK_CONFIG}
+}
+
 function start_kafka(){
 
-    /opt/kafka/bin/zookeeper-server-start.sh /opt/kafka/config/zookeeper.properties &
+    configure_kafka_ip
+
+    /opt/kafka/bin/zookeeper-server-start.sh ${ZOO_CONFIG} &
     ZOO_PID=$!
 
     sleep 5
 
-    /opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server.properties &
+    /opt/kafka/bin/kafka-server-start.sh ${KFK_CONFIG} &
     KFK_PID=$!
 }
 
